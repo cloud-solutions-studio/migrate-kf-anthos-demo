c234d3cdee775e33a091a27dd3d72f48aa6701ad
Michael Intindola <admin@mintindola.workshop.ongcp.co>
2020-10-20 23:42:03 +0000

    Fix load generator script

--- 04-load-generators.sh
+++ 04-load-generators.sh
@@ -1,10 +1,10 @@
+# Connect to the Spring Media cluster
+gcloud container clusters get-credentials ${CLUSTER_NAME} --zone ${CLUSTER_LOCATION}
+
 # Export relevent environment variables
 export ARTIFACT_REGISTRY=${COMPUTE_REGION}-docker.pkg.dev/${PROJECT_ID}/${CLUSTER_NAME}
 export SPRING_BOOKS_URL=$(kubectl get svc istio-ingressgateway -n istio-system -o json | jq -r ".status.loadBalancer.ingress[0].ip")
 export SPRING_MUSIC_URL=$(kf apps -o json | jq -r ".items[0].status.urls[0]")
-
-# Connect to the Spring Media cluster
-gcloud container clusters get-credentials ${CLUSTER_NAME} --zone ${CLUSTER_LOCATION}
 
 # Deploy the Spring Music Load Generator
 cd ~/migrate-kf-anthos-demo/load-generators/spring-music
@@ -15,4 +15,8 @@ gcloud builds submit --tag ${ARTIFACT_REGISTRY}/spring-music-load-generator
 # Deploy the Spring Music Load Generator to the cluster
 envsubst < spring-music-load-generator.yaml | kubectl apply -f -
 
+# Install Siege Load Generator for Spring Books application
+sudo apt-get install -y siege
 
+# Run Siege Load Generator in the background (will continue running if Cloud Shell is closed)
+nohup siege http://${SPRING_BOOKS_URL}/productpage &
