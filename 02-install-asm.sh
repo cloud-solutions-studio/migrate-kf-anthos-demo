f1e99bfd1fe79d7344d1539ceb98212bfac1cfd3
michael-intindola <michael.intindola@gmail.com>
2021-05-10 06:25:34 +0000

    Also install ASM on Kf cluster

--- 02-install-asm.sh
+++ 02-install-asm.sh
@@ -14,13 +14,23 @@ sha256sum -c install_asm.sha256
 # Make the installation script executable
 chmod +x install_asm
 
+echo "Starting installation of ASM on Spring Media Prod and Kf clusters..."
 # Spring Media Production Cluster
 # -------------------------------
-# Configure kubectl command line access
-gcloud container clusters get-credentials ${PROD_CLUSTER_NAME} \
-    --project=${PROJECT_ID} \
-    --zone=${CLUSTER_LOCATION}
-
+# Install Anthos Service Mesh with a fully-managed Control Plane and register to the project's environ
 ./install_asm --mode install --managed -p ${PROJECT_ID} \
     -l ${CLUSTER_LOCATION} -n ${PROD_CLUSTER_NAME} -v \
-    --output_dir ${PROD_CLUSTER_NAME} --enable-all
+    --output_dir ${PROD_CLUSTER_NAME} --enable-all --enable-registration >> "/tmp/spring_media_prod_asm.log" &
+spring_media_prod_asm_pid=$!
+
+# Kf Cluster
+# -------------------------------
+# Install Anthos Service Mesh with a fully-managed Control Plane and register to the project's environ
+./install_asm --mode install --managed -p ${PROJECT_ID} \
+    -l ${CLUSTER_LOCATION} -n ${KF_CLUSTER_NAME} -v \
+    --output_dir ${KF_CLUSTER_NAME} --enable-all >> "/tmp/kf_cluster_asm.log" &
+kf_cluster_asm_pid=$!
+
+wait $spring_media_prod_asm_pid
+wait $kf_cluster_asm_pid
+echo "Installation of ASM on Spring Media Prod and Kf clusters is complete..."
